3693d8c411ffd5dce3673e86ab5b925271772c8c
Kibnet Philosoff <kibnet@hotmail.com>
2022-04-12 23:57:18 +0300

    feat: Добавил Live.Avalonia для горячей перезагрузки

--- src/Unlimotion/App.axaml.cs
+++ src/Unlimotion/App.axaml.cs
@@ -1,12 +1,18 @@
+using System;
+using System.Diagnostics;
+using System.Reactive;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Live.Avalonia;
+using ReactiveUI;
 using Unlimotion.ViewModel;
 using Unlimotion.Views;
 
 namespace Unlimotion
 {
-    public class App : Application
+    public class App : Application, ILiveView
     {
         public override void Initialize()
         {
@@ -15,15 +21,45 @@ namespace Unlimotion
 
         public override void OnFrameworkInitializationCompleted()
         {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            if (Debugger.IsAttached && !IsProduction())
             {
-                desktop.MainWindow = new MainWindow
+                // Here, we create a new LiveViewHost, located in the 'Live.Avalonia'
+                // namespace, and pass an ILiveView implementation to it. The ILiveView
+                // implementation should have a parameterless constructor! Next, we
+                // start listening for any changes in the source files. And then, we
+                // show the LiveViewHost window. Simple enough, huh?
+                var window = new LiveViewHost(this, Console.WriteLine);
+                window.StartWatchingSourceFilesForHotReloading();
+                window.Show();
+            }
+            else
+            {
+                if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
                 {
-                    DataContext = new MainWindowViewModel(),
-                };
+                    desktop.MainWindow = new MainWindow
+                    {
+                        DataContext = new MainWindowViewModel(),
+                    };
+                }
             }
 
+            RxApp.DefaultExceptionHandler = Observer.Create<Exception>(Console.WriteLine);
             base.OnFrameworkInitializationCompleted();
+        }
+        
+        public object CreateView(Window window)
+        {
+            window.DataContext ??= new MainWindowViewModel();
+            return new MainControl();
+        }
+
+        private static bool IsProduction()
+        {
+#if DEBUG
+            return false;
+#else
+        return true;
+#endif
         }
     }
 }
---
+++ src/Unlimotion/Views/MainControl.axaml.cs
@@ -0,0 +1,191 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using Unlimotion.ViewModel;
+
+namespace Unlimotion.Views
+{
+    public partial class MainControl : ReactiveUserControl<MainWindowViewModel>
+    {
+        public MainControl()
+        {
+            InitializeComponent(); 
+            DataContextChanged += MainWindow_DataContextChanged;
+        }
+
+        private void MainWindow_DataContextChanged(object? sender, EventArgs e)
+        {
+            var vm = DataContext as MainWindowViewModel;
+            if (vm != null)
+            {
+                var innerCommand = vm.CreateInner as ReactiveCommand<Unit, Unit>;
+                if (innerCommand != null)
+                {
+                    var subscription = innerCommand.Subscribe(unit =>
+                    {
+                        var toExpand = vm.CurrentItem;
+                        Expand(toExpand);
+                    });
+                    disposables.Add(subscription);
+                }
+            }
+        }
+
+        private List<IDisposable> disposables = new();
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this); 
+
+            AddHandler(DragDrop.DropEvent, Drop);
+            AddHandler(DragDrop.DragOverEvent, DragOver);
+        }
+
+        private const string CustomFormat = "application/xxx-unlimotion-task";
+        
+        private async void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            var dragData = new DataObject();
+            var control = sender as IControl;
+            var dc = control?.DataContext;
+            if (dc == null)
+            {
+                return;
+            }
+
+            dragData.Set(CustomFormat, dc);
+
+            var result = await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Copy | DragDropEffects.Move | DragDropEffects.Link);
+        }
+
+        void DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(CustomFormat))
+            {
+                var control = e.Source as IControl;
+                var task = control?.FindParentDataContext<TaskWrapperViewModel>();
+                var subItem = e.Data.Get(CustomFormat) as TaskWrapperViewModel;
+                if (subItem == null)
+                {
+                    e.DragEffects = DragDropEffects.None;
+                    return;
+                }
+
+                if (e.KeyModifiers == (KeyModifiers.Control | KeyModifiers.Shift))
+                {
+                    e.DragEffects &= DragDropEffects.Copy;
+                }
+                else if (e.KeyModifiers == KeyModifiers.Shift)
+                {
+                    if (subItem.CanMoveInto(task))
+                    {
+                        e.DragEffects &= DragDropEffects.Move;
+                    }
+                    else
+                    {
+                        e.DragEffects = DragDropEffects.None;
+                    }
+                }
+                else if (e.KeyModifiers == KeyModifiers.Control)
+                {
+                    e.DragEffects &= DragDropEffects.Link;
+                }
+                else if (e.KeyModifiers == KeyModifiers.Alt)
+                {
+                    e.DragEffects &= DragDropEffects.Link;
+                }
+                else
+                {
+                    if (subItem.CanMoveInto(task))
+                    {
+                        e.DragEffects &= DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.DragEffects = DragDropEffects.None;
+                    }
+                }
+            }
+            else
+            {
+                e.DragEffects = DragDropEffects.None;
+            }
+        }
+
+        void Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(CustomFormat))
+            {
+                var control = e.Source as IControl;
+                var task = control?.FindParentDataContext<TaskWrapperViewModel>();
+                var subItem = e.Data.Get(CustomFormat) as TaskWrapperViewModel;
+                if (subItem == null)
+                {
+                    e.DragEffects = DragDropEffects.None;
+                    return;
+                }
+                if (e.KeyModifiers == (KeyModifiers.Control | KeyModifiers.Shift))
+                {
+                    e.DragEffects &= DragDropEffects.Copy;
+                    subItem?.TaskItem.CloneInto(task.TaskItem);
+                }
+                else if (e.KeyModifiers == KeyModifiers.Shift)
+                {
+                    if (subItem.CanMoveInto(task))
+                    {
+                        e.DragEffects &= DragDropEffects.Move;
+                        subItem?.TaskItem.MoveInto(task.TaskItem, subItem.Parent?.TaskItem);
+                    }
+                    else
+                    {
+                        e.DragEffects = DragDropEffects.None;
+                    }
+
+                }
+                else if (e.KeyModifiers == KeyModifiers.Control)
+                {
+                    e.DragEffects &= DragDropEffects.Link;
+                    task?.TaskItem.BlockBy(subItem.TaskItem);
+                }
+                else if (e.KeyModifiers == KeyModifiers.Alt)
+                {
+                    e.DragEffects &= DragDropEffects.Link;
+                    subItem?.TaskItem.BlockBy(task.TaskItem);
+                }
+                else
+                {
+                    if (subItem.CanMoveInto(task))
+                    {
+                        e.DragEffects &= DragDropEffects.Copy;
+                        subItem?.TaskItem.CopyInto(task.TaskItem);
+                    }
+                    else
+                    {
+                        e.DragEffects = DragDropEffects.None;
+                    }
+                }
+            }
+            else
+            {
+                e.DragEffects = DragDropEffects.None;
+            }
+        }
+
+        public void Expand(TaskWrapperViewModel task)
+        {
+            var treeView = this.Get<TreeView>("CurrentTree");
+            
+            var treeItem = treeView.ItemContainerGenerator.Containers.FirstOrDefault(info => info.Item == task.Parent);
+            if (treeItem != null)
+            {
+                treeView.ExpandSubTree(treeItem.ContainerControl as TreeViewItem);
+            }
+        }
+    }
+}
