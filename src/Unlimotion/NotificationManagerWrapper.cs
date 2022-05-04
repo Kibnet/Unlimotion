b891a01969a8e18d35ae4328cb1c9f09c814d72d
Kibnet Philosoff <kibnet@hotmail.com>
2022-05-04 17:58:25 +0300

    fix: Поднял версию авалонии, чтобы работало стабильнее на Android, удалил Notification

--- src/Unlimotion/NotificationManagerWrapper.cs
+++ src/Unlimotion/NotificationManagerWrapper.cs
@@ -1,23 +1,24 @@
 using System;
-using Avalonia.Notification;
+//using Avalonia.Notification;
 
 namespace Unlimotion;
 
 public class NotificationManagerWrapperWrapper : Unlimotion.ViewModel.INotificationManagerWrapper
 {
-    public NotificationMessageManager Manager { get; } = new NotificationMessageManager();
+    //public NotificationMessageManager Manager { get; } = new NotificationMessageManager();
 
     public void Ask(string header, string message, Action yesAction, Action noAction = null)
     {
-        Manager.CreateMessage()
-            .Accent("#0078D7")
-            .Animates(true)
-            .Background("#444")
-            .HasBadge("Question")
-            .HasHeader(header)
-            .HasMessage(message)
-            .Dismiss().WithButton("Yes", button => { yesAction?.Invoke();})
-            .Dismiss().WithButton("No", button => { noAction?.Invoke(); })
-            .Queue();
+        //Manager.CreateMessage()
+        //    .Accent("#0078D7")
+        //    .Animates(true)
+        //    .Background("#444")
+        //    .HasBadge("Question")
+        //    .HasHeader(header)
+        //    .HasMessage(message)
+        //    .Dismiss().WithButton("Yes", button => { yesAction?.Invoke();})
+        //    .Dismiss().WithButton("No", button => { noAction?.Invoke(); })
+        //    .Queue();
+        yesAction?.Invoke();
     }
 }
