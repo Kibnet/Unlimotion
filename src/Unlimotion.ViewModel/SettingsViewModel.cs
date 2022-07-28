aebab5c5424941756b720f3470d25a0d3228d390
Kibnet Philosoff <kibnet@hotmail.com>
2022-07-28 18:42:12 +0300

    feat: Добавил новые параметры к настройки

--- src/Unlimotion.ViewModel/SettingsViewModel.cs
+++ src/Unlimotion.ViewModel/SettingsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using PropertyChanged;
+using System.Windows.Input;
 
 namespace Unlimotion.ViewModel;
 
@@ -18,4 +19,32 @@ public class SettingsViewModel
         get => _configuration.GetSection("TaskStorage:Path").Get<string>();
         set => _configuration.GetSection("TaskStorage:Path").Set(value);
     }
+    
+    public string ServerStorageUrl
+    {
+        get => _configuration.GetSection("TaskStorage:URL").Get<string>();
+        set => _configuration.GetSection("TaskStorage:URL").Set(value);
+    }
+
+    public string Login
+    {
+        get => _configuration.GetSection("TaskStorage:Login").Get<string>();
+        set => _configuration.GetSection("TaskStorage:Login").Set(value);
+    }
+
+    //TODO стоит подумать над шифрованным хранением
+    public string Password
+    {
+        get => _configuration.GetSection("TaskStorage:Password").Get<string>();
+        set => _configuration.GetSection("TaskStorage:Password").Set(value);
+    }
+
+    public bool IsServerMode
+    {
+        get => _configuration.GetSection("TaskStorage:IsServerMode").Get<bool>();
+        set => _configuration.GetSection("TaskStorage:IsServerMode").Set(value);
+    }
+
+    public ICommand ConnectCommand { get; set; }
+    public ICommand MigrateCommand { get; set; }
 }
