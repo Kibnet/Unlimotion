3d473909f7ee68f9b2b3b666fa1521c550389a0f
Kibnet Philosoff <kibnet@hotmail.com>
2022-07-28 19:21:13 +0300

    feat: Реализовал переключение между хранилищами задач в зависимости от настроек

--- src/Unlimotion.Desktop/Program.cs
+++ src/Unlimotion.Desktop/Program.cs
@@ -4,6 +4,7 @@ using Avalonia;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
 using System.Reactive;
+using AutoMapper;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Logging;
 using Microsoft.Extensions.Configuration;
@@ -39,15 +40,16 @@ namespace Unlimotion.Desktop
         {
             IConfigurationRoot configuration = WritableJsonConfigurationFabric.Create("Settings.json");
             Locator.CurrentMutable.RegisterConstant(configuration, typeof(IConfiguration));
-            var storagePath = configuration.GetSection("TaskStorage:Path").Get<string>();
-            var taskStorage = new FileTaskStorage(storagePath ?? "Tasks");
-            Locator.CurrentMutable.RegisterConstant<ITaskStorage>(taskStorage);
-            var taskRepository = new TaskRepository(taskStorage);
-            Locator.CurrentMutable.RegisterConstant<ITaskRepository>(taskRepository);
             var mapper = AppModelMapping.ConfigureMapping();
             Locator.CurrentMutable.Register<IMapper>(() => mapper);
+
+            var isServerMode = configuration.GetSection("TaskStorage:IsServerMode").Get<bool>();
+            RegisterStorage(isServerMode, configuration);
+
             var notificationManager = new NotificationManagerWrapperWrapper();
             Locator.CurrentMutable.RegisterConstant<INotificationManagerWrapper>(notificationManager);
+
+
 
 #if DEBUG
             (obj.Instance as App).OnLoaded += (sender, args) =>
@@ -57,9 +59,40 @@ namespace Unlimotion.Desktop
                     desktop.MainWindow?.AttachDevTools();
                     desktop.Windows.FirstOrDefault()?.AttachDevTools();
                 }
+
+                var settingsViewModel = Locator.Current.GetService<SettingsViewModel>();
+                settingsViewModel.ObservableForProperty(m => m.IsServerMode)
+                    .Subscribe(c =>
+                    {
+                        RegisterStorage(c.Value, configuration);
+                    });
+                settingsViewModel.MigrateCommand = ReactiveCommand.CreateFromTask(async () =>
+                {
+                    var storage = Locator.Current.GetService<ITaskStorage>() as ServerTaskStorage;
+                    //storage.
+                });
             };
 
 #endif
+        }
+
+        private static void RegisterStorage(bool isServerMode, IConfigurationRoot configuration)
+        {
+            ITaskStorage taskStorage;
+            if (isServerMode)
+            {
+                var url = configuration.GetSection("TaskStorage:URL").Get<string>();
+                taskStorage = new ServerTaskStorage(url);
+            }
+            else
+            {
+                var storagePath = configuration.GetSection("TaskStorage:Path").Get<string>();
+                taskStorage = new FileTaskStorage(storagePath ?? "Tasks");
+            }
+
+            Locator.CurrentMutable.RegisterConstant<ITaskStorage>(taskStorage);
+            var taskRepository = new TaskRepository(taskStorage);
+            Locator.CurrentMutable.RegisterConstant<ITaskRepository>(taskRepository);
         }
     }
 }
