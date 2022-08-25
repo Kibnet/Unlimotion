59a715ca40c8834f73ded6f83738091aaeac1851
Kibnet Philosoff <kibnet@hotmail.com>
2022-08-25 18:08:57 +0300

    feat: Попробовал запустить на Android

--- src/Unlimotion.Android/SplashActivity.cs
+++ src/Unlimotion.Android/SplashActivity.cs
@@ -1,12 +1,17 @@
 ﻿using Android.App;
 using Android.Content;
-using Application = Android.App.Application;
+using Android.OS;
 
 namespace Unlimotion.Android
 {
     [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
+    	protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
         protected override void OnResume()
         {
             base.OnResume();
