c27cb87039ddf1949cefbb14aa32e419397ae197
Kibnet Philosoff <kibnet@hotmail.com>
2022-09-20 11:16:43 +0300

    fix: Теперь при отжатии фильтров при включеном All работает правильно

--- src/Unlimotion.ViewModel/MainWindowViewModel.cs
+++ src/Unlimotion.ViewModel/MainWindowViewModel.cs
@@ -286,7 +286,7 @@ namespace Unlimotion.ViewModel
                         }
                         foreach (var item in filter.Where(e => e.ShowTasks))
                         {
-                            if (item?.Emoji == null) continue;
+                            if (string.IsNullOrEmpty(item?.Emoji)) continue;
 
                             if (task.GetAllEmoji.Contains(item.Emoji) || (task.Title ?? "").Contains(item.Emoji))
                                 return true;
