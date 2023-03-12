2d0783d7de8ca31e78139020e6cb489a3d7c6e73
Дядькова Анна Сергеевна <dyadkova@triton.ru>
2023-03-12 20:35:37 +0500

    Изменила наименование приложения для Дебиана

--- src/Unlimotion.Desktop/ci/deb/generate-deb-pkg.sh
+++ src/Unlimotion.Desktop/ci/deb/generate-deb-pkg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CSPROJ_PATH="./Unlimotion.Desktop.ForLinuxBuild.csproj"
+CSPROJ_PATH="./Unlimotion.Desktop.ForDebianBuild.csproj"
 
 dotnet tool install --global dotnet-deb
 
@@ -9,5 +9,4 @@ cd ./src/Unlimotion.Desktop
 export PATH=$HOME/.dotnet/tools:$PATH
 dotnet restore $CSPROJ_PATH
 dotnet-deb $CSPROJ_PATH install
-dotnet publish $CSPROJ_PATH -c Release -f net6.0 -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false -p:PublishTrimmed=true
-dotnet-deb $CSPROJ_PATH -c Release -f net6.0 -r linux-x64 -o ./ci/deb
+dotnet msbuild $CSPROJ_PATH -t:CreateDeb -p:OutDir=./ci/deb -p:PublishTrimmed=true -p:PublishSingleFile=true -p:SelfContained=true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false
