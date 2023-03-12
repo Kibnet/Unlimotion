6e88293f1b76ddbb4257da84f6fe775945724cc8
Дядькова Анна Сергеевна <dyadkova@triton.ru>
2023-03-12 20:35:37 +0500

    ci: Изменено наименование приложения для дебиана

--- src/Unlimotion.Desktop/ci/osx/generate-osx-publish.sh
+++ src/Unlimotion.Desktop/ci/osx/generate-osx-publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CSPROJ_PATH="./src/Unlimotion.Desktop/Unlimotion.Desktop.ForMacOSBuild.csproj"
+CSPROJ_PATH="./src/Unlimotion.Desktop/Unlimotion.Desktop.ForMacBuild.csproj"
 
 dotnet restore $CSPROJ_PATH
 dotnet publish $CSPROJ_PATH -c Release -f net6.0 -r osx-x64 -p:PublishSingleFile=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false -p:PublishTrimmed=true
