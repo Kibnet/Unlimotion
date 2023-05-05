1f67d60ae064ae71ad421e5a90c79bc9778ea94b
Дядькова Анна Сергеевна <dyadkova@triton.ru>
2023-05-05 22:49:38 +0500

    Изменила TrimMode

--- src/Unlimotion.Desktop/ci/osx/generate-osx-publish.sh
+++ src/Unlimotion.Desktop/ci/osx/generate-osx-publish.sh
@@ -3,5 +3,5 @@
 CSPROJ_PATH="./src/Unlimotion.Desktop/Unlimotion.Desktop.ForMacBuild.csproj"
 
 dotnet restore $CSPROJ_PATH
-dotnet publish $CSPROJ_PATH -c Release -f net7.0 -r osx-x64 -p:Version=$1 -p:PublishSingleFile=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false -p:PublishTrimmed=true
+dotnet publish $CSPROJ_PATH -c Release -f net7.0 -r osx-x64 -p:Version=$1 -p:PublishSingleFile=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false -p:PublishTrimmed=true -p:TrimMode=Link
 
