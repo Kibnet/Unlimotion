127f1bd498f707b3c37cc22a8eec0a42acbed714
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2023-09-03 20:29:35 +0500

    feat: Изменила триггер обновления PlannedDuration и dotnet чуть подняла

---
+++ src/Unlimotion/Behavior/PlannedDurationBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Unlimotion.Behavior
+{
+    public class LostFocusUpdateBindingBehavior : Behavior<TextBox>
+    {
+        static LostFocusUpdateBindingBehavior()
+        {
+            TextProperty.Changed.Subscribe(e =>
+            {
+                ((LostFocusUpdateBindingBehavior) e.Sender).OnBindingValueChanged();
+            });
+        }
+        
+
+        public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<LostFocusUpdateBindingBehavior, string>(
+            "Text", defaultBindingMode: BindingMode.TwoWay);
+
+        public string Text
+        {
+            get => GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.LostFocus += OnLostFocus;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.LostFocus -= OnLostFocus;
+            base.OnDetaching();
+        }
+        
+        private void OnLostFocus(object? sender, RoutedEventArgs e)
+        {
+            if (AssociatedObject != null)
+                Text = AssociatedObject.Text;
+        }
+        
+        private void OnBindingValueChanged()
+        {
+            if (AssociatedObject != null)
+                AssociatedObject.Text = Text;
+        }
+    }
+}
