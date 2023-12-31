afadebe472d2a0525fd3a4447b2cb17d4d56bcaa
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2023-12-31 22:29:40 +0500

    dotnet restore for deb

--- src/Unlimotion.Desktop/ci/deb/generate-deb-pkg.sh
+++ src/Unlimotion.Desktop/ci/deb/generate-deb-pkg.sh
@@ -7,6 +7,6 @@ dotnet tool install --global dotnet-deb
 cd ./src/Unlimotion.Desktop
 
 export PATH=$HOME/.dotnet/tools:$PATH
-dotnet restore $CSPROJ_PATH
+dotnet restore $CSPROJ_PATH --runtime linux-x64
 dotnet-deb $CSPROJ_PATH install
 dotnet msbuild $CSPROJ_PATH -t:CreateDeb -p:Version=$1 -p:Configuration=Release -p:TargetFramework=net8.0 -p:PublishSingleFile=true -p:SelfContained=true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false
