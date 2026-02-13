25abf35c8e8ce90610c4b63cf2be31cb97a6fb80
Kibnet Philosoff <kibnet@hotmail.com>
2026-02-13 14:58:39 +0300

    refactor: AppNameGenerator заменён на MSBuild подход

--- src/Unlimotion/Services/AppNameDefinitionService.cs
+++ src/Unlimotion/Services/AppNameDefinitionService.cs
@@ -1,13 +1,27 @@
+using System;
+using System.Linq;
+using System.Reflection;
 using Unlimotion.ViewModel;
 
 namespace Unlimotion.Services;
 
 public partial class AppNameDefinitionService : IAppNameDefinitionService
 {
-    private string AppName = "Unlimotion";
+    private static readonly string AppNameSuffix = GetAppNameSuffix();
+    private string AppName = $"Unlimotion{AppNameSuffix}";
     
     public string GetAppName()
     {
         return AppName;
+    }
+
+    private static string GetAppNameSuffix()
+    {
+        const string metadataKey = "UnlimotionAppNameSuffix";
+        var assembly = typeof(AppNameDefinitionService).Assembly;
+        var suffix = assembly
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .FirstOrDefault(attribute => attribute.Key == metadataKey)?.Value;
+        return string.IsNullOrWhiteSpace(suffix) ? string.Empty : $" {suffix}";
     }
 }
