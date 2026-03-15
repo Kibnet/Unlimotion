2c9c75f3f30eec80ff404cc0c2d597069b05a789
Kibnet Fold <Kibnet@hotmail.com>
2026-03-15 23:24:28 +0300

    Android: trust CA bundle and log crashes

--- src/Unlimotion/LibGit2Interop.cs
+++ src/Unlimotion/LibGit2Interop.cs
@@ -3,15 +3,19 @@ using System.Runtime.InteropServices;
 
 namespace Unlimotion;
 
-internal static class LibGit2Interop
+public static class LibGit2Interop
 {
     private const int GitOptSetOwnerValidation = 36;
+    private const int GitOptSetSslCertLocations = 12;
 
     [DllImport("git2", CallingConvention = CallingConvention.Cdecl)]
     private static extern int git_libgit2_init();
 
     [DllImport("git2", CallingConvention = CallingConvention.Cdecl)]
     private static extern int git_libgit2_opts(int option, __arglist);
+
+    [DllImport("git2", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int git_libgit2_opts(int option, IntPtr value1, IntPtr value2);
 
     internal static void DisableOwnerValidationOnAndroid()
     {
@@ -29,6 +33,52 @@ internal static class LibGit2Interop
         }
         catch (Exception)
         {
+        }
+#endif
+    }
+
+    public static void SetSslCertificateLocations(string? certFile, string? certPath)
+    {
+#if ANDROID
+        IntPtr filePtr = IntPtr.Zero;
+        IntPtr pathPtr = IntPtr.Zero;
+
+        try
+        {
+            git_libgit2_init();
+
+            if (!string.IsNullOrWhiteSpace(certFile))
+            {
+                filePtr = Marshal.StringToHGlobalAnsi(certFile);
+            }
+
+            if (!string.IsNullOrWhiteSpace(certPath))
+            {
+                pathPtr = Marshal.StringToHGlobalAnsi(certPath);
+            }
+
+            git_libgit2_opts(GitOptSetSslCertLocations, filePtr, pathPtr);
+        }
+        catch (DllNotFoundException)
+        {
+        }
+        catch (EntryPointNotFoundException)
+        {
+        }
+        catch (Exception)
+        {
+        }
+        finally
+        {
+            if (filePtr != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(filePtr);
+            }
+
+            if (pathPtr != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(pathPtr);
+            }
         }
 #endif
     }
